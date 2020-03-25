922f042338f5e32d0a352f81c9852d0859c0fb27
Lauren Birts <birtslauren@gmail.com>
2020-03-25 02:32:41 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -22,3 +22,12 @@ def reduce_to_all_true(source_array)
 end
 
 def reduce_to_any_true(source_array)
+    index = 0
+  while index < source_array.length do 
+    if !!source_array[index] == true
+      return true
+    end 
+    index += 1 
+  end 
+  false
+end
