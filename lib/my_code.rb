1f1125f83a2f9631bd425560d37ee89248eceb91
Lauren Birts <birtslauren@gmail.com>
2020-03-25 02:16:35 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -3,4 +3,6 @@ def map_to_negativize(source_array)
   source_array.map { |element| -(element) }
 end
 
-def map_to_no_change
+def map_to_no_change(source_array)
+  source_array.map { |element| (element) }
+end
