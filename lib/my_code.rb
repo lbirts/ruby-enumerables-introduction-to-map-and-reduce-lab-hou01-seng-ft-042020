d9fe438d1c0f1e799b7cefc68caf9b6d1a6c4c09
Lauren Birts <birtslauren@gmail.com>
2020-03-25 02:27:35 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -13,6 +13,6 @@ def map_to_square(source_array)
   source_array.map { |element| element ** 2 }
 end
 
-def reduct_to_total(source_array, starting_point)
+def reduct_to_total(source_array, starting_point = 0)
   source_array.reduce(starting_point) { |sum, num| sum + num }
 end
