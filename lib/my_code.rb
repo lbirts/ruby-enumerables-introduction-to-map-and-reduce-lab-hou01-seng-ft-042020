c093d74fcf4d628b1fb5eb03427265ee848f5dda
Lauren Birts <birtslauren@gmail.com>
2020-03-25 02:22:34 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -10,5 +10,5 @@ def map_to_double(source_array)
   source_array.map { |element| element * 2 }
 end
 def map_to_square(source_array)
-  source_array.map { |element| element %2 }
+  source_array.map { |element| element ** 2 }
 end
