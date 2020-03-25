e4049d48ef5e968d6b0e5e2e090afc4fdb5294d4
Lauren Birts <birtslauren@gmail.com>
2020-03-25 02:20:09 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -7,4 +7,5 @@ def map_to_no_change(source_array)
   source_array
 end
 def map_to_double(source_array)
-  source_array.map { |element| element *2 }
+  source_array.map { |element| element * 2 }
+end
