0584ab36a026d5f0b335bea2790c78008819e7a5
Lauren Birts <birtslauren@gmail.com>
2020-03-25 02:32:51 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,33 @@
 # My Code here....
+def map_to_negativize(source_array)
+  source_array.map { |element| -(element) }
+end
+
+def map_to_no_change(source_array)
+  source_array
+end
+def map_to_double(source_array)
+  source_array.map { |element| element * 2 }
+end
+def map_to_square(source_array)
+  source_array.map { |element| element ** 2 }
+end
+
+def reduce_to_total(source_array, starting_point = 0)
+  source_array.reduce(starting_point) { |sum, num| sum + num }
+end
+
+def reduce_to_all_true(source_array)
+  source_array.reduce { |beTrue, beFalse| beTrue && beFalse}
+end
+
+def reduce_to_any_true(source_array)
+    index = 0
+  while index < source_array.length do 
+    if !!source_array[index] == true
+      return true
+    end 
+    index += 1 
+  end 
+  false
+end
